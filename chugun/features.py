c4e41b1d0f88134972045ff2891bba2018e97d38
Ivan <1vanmoshkovv@mail.ru>
2021-10-30 12:07:47 +0300

    spasibo chto ne v telegrame

---
+++ chugun/features.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+from datetime import datetime
+
+def process_data(chugun_train, chugun_test, drop_outliers=False):
+    train = chugun_train.copy()
+    test = chugun_test.copy()
+    if not drop_outliers:
+        train.loc[train.VES==0, 'VES'] = train.VES.mean()
+        test.loc[test.VES==0, 'VES'] = train.VES.mean()
+        train.loc[train['T']==0, 'T'] = train['T'].mean()
+        test.loc[test['T']==0, 'T'] = train['T'].mean()
+    else:
+        train = train[(train.VES!=0)&(train['T']!=0)]
+        test.loc[test.VES==0, 'VES'] = train.VES.median()
+        test.loc[test['T']==0, 'T'] = train['T'].median()
+
+    train['is_train'] = 1
+    test['is_train'] = 0
+    train['DATA_ZAMERA'] = pd.to_datetime(train['DATA_ZAMERA'])
+    test['DATA_ZAMERA'] = pd.to_datetime(test['DATA_ZAMERA'])
+    data = pd.concat([train, test], ignore_index=True)
+    
+    elements = ['SI', 'MN', 'S', 'P', 'CR', 'NI', 'CU', 'V', 'TI']
+    for col in elements:
+        data[col.lower() + '_portion'] = data[col] / data['VES']
+    
+    data['total_seconds'] = (data.DATA_ZAMERA - datetime(1970, 1, 1)).dt.total_seconds()
+
+    ok_cols = [
+        'NPLV', 'VES', 'T', 'SI', 'MN', 'S', 'P', 'CR', 'NI', 'CU', 'V', 'TI',
+        'si_portion', 'mn_portion', 's_portion', 'p_portion', 'cr_portion',
+        'ni_portion', 'cu_portion', 'v_portion', 'ti_portion', 'total_seconds'
+    ]
+    num_cols = ok_cols
+    cat_cols = []
+    
+    return data[ok_cols], num_cols, cat_cols
