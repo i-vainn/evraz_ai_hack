33be17e4dc20065b720c4a834863dac31a9ec0e6
sizovk <sizovki@yandex.ru>
2021-10-30 11:07:49 +0300

    added sum feature

--- gas/features.py
+++ gas/features.py
@@ -13,8 +13,9 @@ def generate_gas_features(df):
     std_features = df.groupby(by="NPLV").std().rename(columns=lambda col : "gas_std_" + col)
     
     fraction_columns = ['O2', 'N2', 'H2', 'CO2', 'CO', 'AR']
-    volume_df = df[["NPLV"]].join(df[fraction_columns].multiply(df["V"], axis="index"))
+    volume_df = df[["NPLV"]].join(df[fraction_columns].multiply(df["V"] / 100, axis="index"))
+    sum_volume_features = volume_df.groupby(by="NPLV").sum().rename(columns=lambda col : "gas_sum_volume_" + col)
     mean_volume_features = volume_df.groupby(by="NPLV").mean().rename(columns=lambda col : "gas_mean_volume_" + col)
     std_volume_features = volume_df.groupby(by="NPLV").std().rename(columns=lambda col : "gas_std_volume_" + col)
     
-    return pd.concat([duration_feature, mean_features, std_features, mean_volume_features, std_volume_features], axis=1)
+    return pd.concat([duration_feature, mean_features, std_features, sum_volume_features, mean_volume_features, std_volume_features], axis=1)
