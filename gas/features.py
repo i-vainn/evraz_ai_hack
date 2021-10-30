541e9c623590ac459d87d9097b0372d6e695a66b
sizovk <sizovki@yandex.ru>
2021-10-30 10:57:04 +0300

    added volume features

--- gas/features.py
+++ gas/features.py
@@ -8,6 +8,13 @@ def generate_gas_features(df):
     :return: dataframe with features with NPLV as index
     """
     duration_feature = df[["NPLV", "Time"]].groupby(by="NPLV").count().rename(columns={"Time": "gas_duration"})
-    mean_features = gas_test.groupby(by="NPLV").mean().rename(columns=lambda col : "gas_mean_" + col)
-    std_features = gas_test.groupby(by="NPLV").std().rename(columns=lambda col : "gas_std_" + col)
-    return pd.concat([duration_feature, mean_features, std_features], axis=1)
+
+    mean_features = df.groupby(by="NPLV").mean().rename(columns=lambda col : "gas_mean_" + col)
+    std_features = df.groupby(by="NPLV").std().rename(columns=lambda col : "gas_std_" + col)
+    
+    fraction_columns = ['O2', 'N2', 'H2', 'CO2', 'CO', 'AR']
+    volume_df = df[["NPLV"]].join(df[fraction_columns].multiply(df["V"], axis="index"))
+    mean_volume_features = volume_df.groupby(by="NPLV").mean().rename(columns=lambda col : "gas_mean_volume_" + col)
+    std_volume_features = volume_df.groupby(by="NPLV").std().rename(columns=lambda col : "gas_std_volume_" + col)
+    
+    return pd.concat([duration_feature, mean_features, std_features, mean_volume_features, std_volume_features], axis=1)
